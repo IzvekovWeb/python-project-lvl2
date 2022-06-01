5014d61f54d305fb82c92ab6eb49898005977070
Alexander Izvekov <izvekov.web@yandex.ru>
2022-06-01 16:00:43 +0300

    Fix lint

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,9 +1,8 @@
 from gendiff.parsers.parse import parse_by_extension
 
 
-def generate_diff(path_file1, path_file2):
+def generate_diff(path_file1, path_file2): # noqa
     file1, file2 = parse_by_extension(path_file1, path_file2)
-
 
     def make_diff(struct_1, struct_2):
         f1_keys = list(struct_1.keys())
@@ -18,23 +17,23 @@ def generate_diff(path_file1, path_file2):
                     # Ключ есть в обоих файлах
                     if struct_1[key] == struct_2[key]:
                         # Значения равны
-                        res[f"  {key}"] =  add_space(struct_1[key])
+                        res[f"  {key}"] = add_space(struct_1[key])
                     else:
                         # Значения различные
-                        if isinstance(struct_1[key], dict) \
-                            and isinstance(struct_2[key], dict):
+                        if isinstance(struct_1[key], dict) and \
+                                isinstance(struct_2[key], dict):
 
                             subval = make_diff(struct_1[key], struct_2[key])
-                            res[f"  {key}"] =  subval
+                            res[f"  {key}"] = subval
                         else:
-                            res[f"- {key}"] =  add_space(struct_1[key])
-                            res[f"+ {key}"] =  add_space(struct_2[key])
+                            res[f"- {key}"] = add_space(struct_1[key])
+                            res[f"+ {key}"] = add_space(struct_2[key])
                 else:
                     # Ключ только в 1м файле
-                    res[f"- {key}"] =  add_space(struct_1[key])
+                    res[f"- {key}"] = add_space(struct_1[key])
             else:
                 # Ключ только во 2м файле
-                res[f"+ {key}"] =  add_space(struct_2[key])
+                res[f"+ {key}"] = add_space(struct_2[key])
 
         return res
 
@@ -52,5 +51,3 @@ def add_space(value):
                 result[f"  {el}"] = val
         return result
     return value
-
-print(generate_diff('gendiff/parsers/file1.json', 'gendiff/parsers/file2.json'))
